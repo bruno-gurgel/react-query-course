251df90b08e79a19a6913b387086dced69f4468f
Bruno Gurgel <bmg1612@gmail.com>
2022-10-19 09:56:47 -0300

    First lesson

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import IssueItem from "./IssuesItem";
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
+
   return (
     <div>
       <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {issuesQuery.data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              commentCount={issue.comments.length}
+              {...issue}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
